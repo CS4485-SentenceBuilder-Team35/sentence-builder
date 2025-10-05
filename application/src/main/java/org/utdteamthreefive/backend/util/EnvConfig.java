9769eab69391a02a4546527d31b08fb21bcf0e92
ZaeemR2000 <raszaeem@gmail.com>
2025-10-04 19:21:43 -0700

    feat: add EnvConfig for .env support

---
+++ application/src/main/java/org/utdteamthreefive/backend/util/EnvConfig.java
@@ -0,0 +1,23 @@
+package org.utdteamthreefive.backend.util;
+import io.github.cdimascio.dotenv.Dotenv;
+
+/**
+ * Loads and provides access to environment variables from the .env file.
+ * <p>
+ * <pre>
+ * String user = EnvConfig.get("<variable from .env file>");
+ * </pre>
+ * @author Zaeem Rashid
+ */
+public class EnvConfig {
+    private static final Dotenv dotenv = Dotenv.load();
+
+    /**
+     * Get a value from the .env file by its key.
+     * @param key the name of the environment variable
+     * @return the value of the variable, or null if not found
+     */
+    public static String get(String key) {
+        return dotenv.get(key);
+    }
+}

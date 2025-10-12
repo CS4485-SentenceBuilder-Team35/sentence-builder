9eee646bd3e35bb129a3a0f0675065fbd8fafd36
Ice <ice.baldivas@gmail.com>
2025-10-11 22:22:59 -0500

    Initialize UI elements that matches the mockup

--- application/src/main/java/org/utdteamthreefive/ui/Main.java
+++ application/src/main/java/org/utdteamthreefive/ui/Main.java
@@ -2,6 +2,7 @@ package org.utdteamthreefive.ui;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -11,7 +12,8 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("main-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root, 320, 240);
         stage.setTitle("Sentence Builder");
         stage.setScene(scene);
         stage.show();

0cc2f53afa966a4e843dc5e96eb1e00d1be98ef9
Ice <ice.baldivas@gmail.com>
2025-10-11 22:22:59 -0500

    Initialize UI elements that matches the mockup

--- application/src/main/java/org/utdteamthreefive/ui/MainController.java
+++ application/src/main/java/org/utdteamthreefive/ui/MainController.java
@@ -1,17 +1,41 @@
 package org.utdteamthreefive.ui;
 
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
 import org.utdteamthreefive.backend.SampleClass;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 public class MainController {
-    @FXML
-    private Label welcomeText;
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+//    @FXML
+//    private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
         SampleClass sampleClass = new SampleClass();
-        welcomeText.setText(sampleClass.getGreeting());
+//        welcomeText.setText(sampleClass.getGreeting());
+    }
+
+    @FXML
+    protected void onSwitchToTableView(ActionEvent event) {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("table-view.fxml"));
+        try {
+            root = fxmlLoader.load();
+            stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+            scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (java.io.IOException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ application/src/main/java/org/utdteamthreefive/ui/Table.java
@@ -0,0 +1,5 @@
+package org.utdteamthreefive.ui;
+
+public class Table {
+
+}

483ef3b4a21b1d7c4b6767aed099c5055b2b0fb6
Ice <ice.baldivas@gmail.com>
2025-10-15 21:57:10 -0500

    Create table dynamically to see sample data

--- application/src/main/java/org/utdteamthreefive/ui/MainController.java
+++ application/src/main/java/org/utdteamthreefive/ui/MainController.java
@@ -2,13 +2,11 @@ package org.utdteamthreefive.ui;
 
 import javafx.event.ActionEvent;
 import javafx.scene.Node;
-import org.utdteamthreefive.backend.SampleClass;
-
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
 
 public class MainController {
@@ -16,24 +14,28 @@ public class MainController {
     private Scene scene;
     private Parent root;
 
-//    @FXML
-//    private Label welcomeText;
-
-    @FXML
-    protected void onHelloButtonClick() {
-        SampleClass sampleClass = new SampleClass();
-//        welcomeText.setText(sampleClass.getGreeting());
-    }
-
+    /**
+     * @author Rommel Isaac Baldivas
+     * @param event
+     */
     @FXML
     protected void onSwitchToTableView(ActionEvent event) {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("table-view.fxml"));
         try {
             root = fxmlLoader.load();
-            stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
             scene = new Scene(root);
             stage.setScene(scene);
+
+            Table table = new Table();
+
+            // Query the FlowPane Node so that the Table can be added to it
+            FlowPane flowPane = (FlowPane) scene.lookup(".main-flow-pane");
+            // flowPane.getChildren().add(table.getTableView());
+            // FlowPane.setMargin(table.getTableView(), new Insets(16.0, 0, 16.0, 0));
+
             stage.show();
+
         } catch (java.io.IOException e) {
             e.printStackTrace();
         }
--- application/src/main/java/org/utdteamthreefive/ui/Table.java
+++ application/src/main/java/org/utdteamthreefive/ui/Table.java
@@ -1,5 +1,74 @@
+/**
+ * @author Rommel Isaac Baldivas
+ */
+
 package org.utdteamthreefive.ui;
 
-public class Table {
+import java.util.ArrayList;
+import java.util.Arrays;
 
+import org.utdteamthreefive.backend.models.Word;
+import org.utdteamthreefive.backend.models.enums.WordType;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+public class Table {
+    TableView<Word> table;
+
+    // Sample data for now
+    ObservableList<Word> data = FXCollections.observableArrayList(new Word(1, "hello", 25, 5, 3, WordType.alpha),
+            new Word(2, "world", 30, 8, 4, WordType.alpha), new Word(3, "the", 150, 45, 12, WordType.alpha),
+            new Word(4, "quick", 15, 2, 1, WordType.alpha), new Word(5, "brown", 12, 3, 2, WordType.alpha),
+            new Word(6, "fox", 8, 1, 1, WordType.alpha), new Word(7, "jumps", 6, 2, 0, WordType.alpha),
+            new Word(8, "over", 20, 4, 2, WordType.alpha), new Word(9, "lazy", 10, 1, 1, WordType.alpha),
+            new Word(10, "dog", 18, 3, 2, WordType.alpha), new Word(11, "!", 75, 0, 25, WordType.misc),
+            new Word(12, ".", 200, 0, 80, WordType.misc), new Word(13, "?", 35, 0, 15, WordType.misc));
+
+    public Table() {
+        table = new TableView<Word>();
+        table.setItems(data);
+        table.getStyleClass().add("main-table");
+        table.getColumns().setAll(setupColumns());
+        // Prevent the table from being editable
+        table.setEditable(false);
+    }
+
+    public ArrayList<TableColumn<Word, ?>> setupColumns() {
+        // Setup the columns
+        TableColumn<Word, String> wordTokenCol = new TableColumn<Word, String>("Text");
+        wordTokenCol.setPrefWidth(400);
+        wordTokenCol.setCellValueFactory(new PropertyValueFactory<>("wordToken"));
+        wordTokenCol.getStyleClass().add("column-primary");
+
+        TableColumn<Word, Integer> totalCountCol = new TableColumn<Word, Integer>("Total Count");
+        totalCountCol.setPrefWidth(90);
+        totalCountCol.setCellValueFactory(new PropertyValueFactory<>("totalCount"));
+        totalCountCol.getStyleClass().add("column-secondary");
+
+        TableColumn<Word, Integer> startCountCol = new TableColumn<Word, Integer>("Start Count");
+        startCountCol.setPrefWidth(90);
+        startCountCol.setCellValueFactory(new PropertyValueFactory<>("startCount"));
+        startCountCol.getStyleClass().add("column-primary");
+
+        TableColumn<Word, Integer> endCountCol = new TableColumn<Word, Integer>("End Count");
+        endCountCol.setPrefWidth(90);
+        endCountCol.setCellValueFactory(new PropertyValueFactory<>("endCount"));
+        endCountCol.getStyleClass().add("column-secondary");
+
+        TableColumn<Word, String> typeCol = new TableColumn<Word, String>("Type");
+        typeCol.setPrefWidth(200);
+        typeCol.setCellValueFactory(new PropertyValueFactory<>("type"));
+        typeCol.getStyleClass().add("column-primary");
+
+        return new ArrayList<TableColumn<Word, ?>>(
+                Arrays.asList(wordTokenCol, totalCountCol, startCountCol, endCountCol, typeCol));
+    }
+
+    public TableView<Word> getTableView() {
+        return table;
+    }
 }

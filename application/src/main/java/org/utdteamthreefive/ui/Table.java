5ad0216f2004806b9dc3f79fd3901648057db7f8
Ice <ice.baldivas@gmail.com>
2025-10-19 22:22:52 -0500

    Query word table and display in UI

--- application/src/main/java/org/utdteamthreefive/ui/Table.java
+++ application/src/main/java/org/utdteamthreefive/ui/Table.java
@@ -4,17 +4,19 @@
 
 package org.utdteamthreefive.ui;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.utdteamthreefive.backend.models.Word;
 import org.utdteamthreefive.backend.models.enums.WordType;
+import org.utdteamthreefive.backend.util.DatabaseManager;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.scene.control.TableView.TableViewSelectionModel;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.util.converter.IntegerStringConverter;
@@ -23,15 +25,11 @@ public class Table {
     TableView<Word> table;
 
     // Sample data for now
-    ObservableList<Word> data = FXCollections.observableArrayList(new Word(1, "hello", 25, 5, 3, WordType.alpha),
-            new Word(2, "world", 30, 8, 4, WordType.alpha), new Word(3, "the", 150, 45, 12, WordType.alpha),
-            new Word(4, "quick", 15, 2, 1, WordType.alpha), new Word(5, "brown", 12, 3, 2, WordType.alpha),
-            new Word(6, "fox", 8, 1, 1, WordType.alpha), new Word(7, "jumps", 6, 2, 0, WordType.alpha),
-            new Word(8, "over", 20, 4, 2, WordType.alpha), new Word(9, "lazy", 10, 1, 1, WordType.alpha),
-            new Word(10, "dog", 18, 3, 2, WordType.alpha), new Word(11, "!", 75, 0, 25, WordType.misc),
-            new Word(12, ".", 200, 0, 80, WordType.misc), new Word(13, "?", 35, 0, 15, WordType.misc));
+    ObservableList<Word> data = FXCollections.observableArrayList();
 
     ObservableList<Word> selectedItems;
+
+    private Connection conn;
 
     public Table() {
         table = new TableView<Word>();
@@ -43,6 +41,29 @@ public class Table {
 
         // Allow table to expand when columns are resized
         table.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY_ALL_COLUMNS);
+
+        // Open DB connection and query for words
+        conn = DatabaseManager.open();
+        if (conn == null) {
+            System.err.println("❌ Failed to open database connection");
+            return;
+        }
+        String wordQuery = "SELECT word_id, word_token, total_count, start_count, end_count, type_ FROM WORD";
+        try {
+            PreparedStatement selectStatement = conn.prepareStatement(wordQuery);
+            var rs = selectStatement.executeQuery();
+            data.clear();
+            while (rs.next()) {
+                WordType type = WordType.valueOf(rs.getString("type_").toLowerCase());
+                Word word = new Word(rs.getInt("word_id"), rs.getString("word_token"), rs.getInt("total_count"),
+                        rs.getInt("start_count"), rs.getInt("end_count"), type);
+                data.add(word);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            DatabaseManager.close(conn);
+        }
     }
 
     public ArrayList<TableColumn<Word, ?>> setupColumns() {

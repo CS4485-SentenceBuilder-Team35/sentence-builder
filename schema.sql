6f83904d48bfdfc48c9222440c19a34cf64bc260
Rommel Isaac Baldivas <38993027+ice-baldivas@users.noreply.github.com>
2025-10-21 23:26:43 -0500

    Revert "Query word table and display in UI"
    This reverts commit 9aefdf2fa5375c3eed556ed4075470f5bcb33c29.

--- schema.sql
+++ schema.sql
@@ -1,26 +1,30 @@
 -- AIDEN MARTINEZ
-drop database SentenceBuilder;
 create database IF NOT EXISTS SentenceBuilder;
 use SentenceBuilder;
+
 CREATE TABLE IF NOT EXISTS FILES(
-    file_id INT PRIMARY KEY AUTO_INCREMENT,
-    file_path VARCHAR(500) UNIQUE,
+	file_id INT PRIMARY KEY AUTO_INCREMENT,
+	file_path VARCHAR(500) UNIQUE,
     word_count INT,
     date_imported DATE
 );
+
+
 CREATE TABLE IF NOT EXISTS WORD(
-    word_id INT PRIMARY KEY AUTO_INCREMENT,
-    word_token VARCHAR(50) UNIQUE,
+    word_id INT PRIMARY KEY,
+    word_token VARCHAR(50),
     total_count INT,
     start_count INT,
     end_count INT,
     type_ ENUM('alpha', 'misc'),
     CHECK (total_count > 0)
 );
+
 CREATE TABLE IF NOT EXISTS WORD_FOLLOW(
-    from_word_id INT,
+	from_word_id INT,
     to_word_id INT,
     total_count INT,
+    
     PRIMARY KEY(from_word_id, to_word_id),
     FOREIGN KEY (from_word_id) REFERENCES WORD(word_id) ON DELETE CASCADE,
     FOREIGN KEY (to_word_id) REFERENCES WORD(word_id) ON DELETE CASCADE

39e1bb52111589736b70ed7601dfc68b36b7af14
Rommel Isaac Baldivas <38993027+ice-baldivas@users.noreply.github.com>
2025-10-21 23:37:08 -0500

    UI Table for database (#45)
    * Initialize UI elements that matches the mockup
    
    * Create table dynamically to see sample data
    
    * Update styling of table
    
    * Query word table and display in UI
    
    * Code improvement
    
    * Remove unneccessary connection check

--- application/src/main/java/org/utdteamthreefive/ui/MainController.java
+++ application/src/main/java/org/utdteamthreefive/ui/MainController.java
@@ -1,17 +1,44 @@
 package org.utdteamthreefive.ui;
 
-import org.utdteamthreefive.backend.SampleClass;
-
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
 
 public class MainController {
-    @FXML
-    private Label welcomeText;
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
 
+    /**
+     * @author Rommel Isaac Baldivas
+     * @param event
+     */
     @FXML
-    protected void onHelloButtonClick() {
-        SampleClass sampleClass = new SampleClass();
-        welcomeText.setText(sampleClass.getGreeting());
+    protected void onSwitchToTableView(ActionEvent event) {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("table-view.fxml"));
+        try {
+            root = fxmlLoader.load();
+            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            scene = new Scene(root);
+            stage.setScene(scene);
+
+            Table table = new Table();
+
+            // Query the FlowPane Node so that the Table can be added to it
+            FlowPane flowPane = (FlowPane) scene.lookup(".main-flow-pane");
+            flowPane.getChildren().add(table.getTableView());
+            FlowPane.setMargin(table.getTableView(), new Insets(16.0, 0, 16.0, 0));
+
+            stage.show();
+
+        } catch (java.io.IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- schema.sql
+++ schema.sql
@@ -1,30 +1,26 @@
 -- AIDEN MARTINEZ
+drop database IF EXISTS SentenceBuilder;
 create database IF NOT EXISTS SentenceBuilder;
 use SentenceBuilder;
-
 CREATE TABLE IF NOT EXISTS FILES(
-	file_id INT PRIMARY KEY AUTO_INCREMENT,
-	file_path VARCHAR(500) UNIQUE,
+    file_id INT PRIMARY KEY AUTO_INCREMENT,
+    file_path VARCHAR(500) UNIQUE,
     word_count INT,
     date_imported DATE
 );
-
-
 CREATE TABLE IF NOT EXISTS WORD(
-    word_id INT PRIMARY KEY,
-    word_token VARCHAR(50),
+    word_id INT PRIMARY KEY AUTO_INCREMENT,
+    word_token VARCHAR(50) UNIQUE,
     total_count INT,
     start_count INT,
     end_count INT,
     type_ ENUM('alpha', 'misc'),
     CHECK (total_count > 0)
 );
-
 CREATE TABLE IF NOT EXISTS WORD_FOLLOW(
-	from_word_id INT,
+    from_word_id INT,
     to_word_id INT,
     total_count INT,
-    
     PRIMARY KEY(from_word_id, to_word_id),
     FOREIGN KEY (from_word_id) REFERENCES WORD(word_id) ON DELETE CASCADE,
     FOREIGN KEY (to_word_id) REFERENCES WORD(word_id) ON DELETE CASCADE

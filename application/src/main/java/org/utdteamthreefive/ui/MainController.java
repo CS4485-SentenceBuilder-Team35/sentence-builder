0ac91cf91bc2900950d2a5cd4a4e323b84d7be83
Ice <ice.baldivas@gmail.com>
2025-10-27 15:42:41 -0500

    Add tab pane and styling

--- application/src/main/java/org/utdteamthreefive/ui/MainController.java
+++ application/src/main/java/org/utdteamthreefive/ui/MainController.java
@@ -1,26 +1,32 @@
 package org.utdteamthreefive.ui;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.scene.Node;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.layout.HBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TabPane;
 import javafx.scene.layout.FlowPane;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
-import org.utdteamthreefive.backend.FileParseHandle;
+import org.utdteamthreefive.backend.util.FileParseHandle;
 import org.utdteamthreefive.backend.SampleClass;
 
 import javax.swing.*;
 import java.io.File;
 import java.util.List;
 import javafx.scene.layout.VBox;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class MainController {
+public class MainController implements Initializable {
     private Stage stage;
     private Scene scene;
     private Parent root;
@@ -29,6 +35,7 @@ public class MainController {
     private HBox fileRow; // fx:id="fileRow" in FXML
     private ProgressBar progressBar; // fx:id="progressBar" in FXML
     private VBox uploadContainer;
+    private TabPane tabPane;
 
     /**
      * @author Rommel Isaac Baldivas
@@ -86,15 +93,59 @@ public class MainController {
     protected void onGenerateSentenceButtonClick(ActionEvent event) {
     }
 
-    public void initialize() {
-        // Add sample file tabs for testing
-        addFileTab("example.txt");
-        addFileTab("report.pdf");
+    // public void initialize() {
+    // // Add sample file tabs for testing
+    // addFileTab("example.txt");
+    // addFileTab("report.pdf");
+    // }
+
+    // public FileTab addFileTab(String fileName) {
+    // FileTab fileTab = new FileTab(fileName);
+    // uploadContainer.getChildren().add(fileTab);
+    // return fileTab;
+    // }
+
+    /**
+     * @author Rommel Isaac Baldivas
+     */
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // Set up responsive tab widths if TabPane exists
+        if (tabPane != null) {
+            setupResponsiveTabWidths();
+
+            // Use Platform.runLater to ensure the TabPane is in the scene graph
+            Platform.runLater(() -> {
+                scene = tabPane.getScene();
+                if (scene != null) {
+                    // Query the FlowPane Node so that the Table can be added to it
+                    Table table = new Table();
+                    FlowPane flowPane = (FlowPane) scene.lookup(".table-flow-pane");
+                    if (flowPane != null) {
+                        flowPane.getChildren().add(table.getTableView());
+                        FlowPane.setMargin(table.getTableView(), new Insets(16.0, 0, 16.0, 0));
+                    }
+                }
+            });
+        }
     }
 
-    public FileTab addFileTab(String fileName) {
-        FileTab fileTab = new FileTab(fileName);
-        uploadContainer.getChildren().add(fileTab);
-        return fileTab;
+    /**
+     * @author Rommel Isaac Baldivas
+     *         The event listener will listen for width changes and update tab
+     *         widths accordingly
+     */
+    private void setupResponsiveTabWidths() {
+        tabPane.widthProperty().addListener((observable, oldValue, newValue) -> {
+            double availableWidth = newValue.doubleValue() - tabPane.getInsets().getLeft()
+                    - tabPane.getInsets().getRight();
+            int tabCount = tabPane.getTabs().size();
+
+            if (tabCount > 0 && availableWidth > 0) {
+                double tabWidth = availableWidth / tabCount;
+                tabPane.setTabMinWidth(tabWidth);
+                tabPane.setTabMaxWidth(Double.MAX_VALUE); // No max width limit
+            }
+        });
     }
 }
